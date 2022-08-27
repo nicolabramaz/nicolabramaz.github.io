d711b8128e02788b3f5c1c774d536dbe4aac5754
Nicola Bramaz <nicola@n23dev.ch>
2022-08-27 17:23:55 +0200

    Put the site

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# nicolabramazch
+# nicolabramaz.ch
+C'est un site du profit professionnel de Nicola Bramaz par sa parcours.
+
+## Template
+### Astral by HTML5 UP
+[html5up.net](https://html5up.net) | Gratuit pour un usage personnel et commercial sous la [CCA 3.0 license] (https://html5up.net/license)
+
+Premier du genre sur H5U! Astral dispose d'un design plat et minimaliste, entièrement animé
+interface (avec des replis de noscript), et un style pour tous les éléments de la page de base (y compris blockquotes, tableaux et listes). Prendre plaisir!
+
+Les commentaires, les rapports de bogues et les commentaires sont non seulement les bienvenus, mais fortement encouragés via le site de [html5up.net](https://html5up.net) :)
+
+AJ [aj@lkn.io](mailto:aj@lkn.io) | [@ajlkn in Twitter](https://twitter.com/ajlkn) | [@ajlkn in Github](https://github.com/ajlkn)
+
+#### Extensions inclus du site
+**Icônes** :
+- Font Awesome (fortawesome.github.com/Font-Awesome)
+
+**Autres** :
+- [jQuery](https://github.com/jquery)
+- [html5shiv.js](https://github.com/aFarkas/html5shiv)
+- [background-size polyfill](https://github.com/louisremi)
+- [Repond.js](https://github.com/scottjehl/Respond)
+- [Skel](https://github.com/ajlkn/skel)
+
+### Personnalisation du site par Nicola Bramaz
+J'aime vraiment beaucoup les sites qui sont fait par html5up.net car ce sont les templates très jolis. C'est pourquoi, j'ai fait le choix de template Astral car c'est un site simplifié qui est parfait pour mon site.
+
+Je trouve un travail assez facile et rapide pour un site minimal. C'est la rasion quel que j'aime le travail de AJ. Je vous conseille à vous amuser de faire aux tests par ses templates gratuits.
+
+#### Mon Travail du développement web
+- Matériel informatique actuellement : Macbook Pro 14 pouces M1 Pro - 2021
+- Logiciel d'édition web : [Code Visuel Code](https://code.visualstudio.com/)
+- Navigateur : [Firefox Developer Edition](https://www.mozilla.org/fr/firefox/developer/)
+
+#### Scripts ajoutée
+- [Typewriter Effect](https://css-tricks.com/snippets/css/typewriter-effect/)
+- [Image Overlay Fade](https://www.w3schools.com/howto/howto_css_image_overlay.asp)
+- [CSS Timeline](https://codepen.io/hansf/pen/JvZpzL)
+
+## Hébergement
+Le site **nicolabramaz.ch** est stocké sur [Github](https://github.com/) qui est un hébergement gratuit.
---
+++ assets/js/main.js
@@ -0,0 +1,275 @@
+/*
+	Astral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var $window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$main = $('#main'),
+		$panels = $main.children('.panel'),
+		$nav = $('#nav'), $nav_links = $nav.children('a');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ '361px',   '736px'  ],
+			xsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Nav.
+		$nav_links
+			.on('click', function(event) {
+
+				var href = $(this).attr('href');
+
+				// Not a panel link? Bail.
+					if (href.charAt(0) != '#'
+					||	$panels.filter(href).length == 0)
+						return;
+
+				// Prevent default.
+					event.preventDefault();
+					event.stopPropagation();
+
+				// Change panels.
+					if (window.location.hash != href)
+						window.location.hash = href;
+
+			});
+
+	// Panels.
+
+		// Initialize.
+			(function() {
+
+				var $panel, $link;
+
+				// Get panel, link.
+					if (window.location.hash) {
+
+				 		$panel = $panels.filter(window.location.hash);
+						$link = $nav_links.filter('[href="' + window.location.hash + '"]');
+
+					}
+
+				// No panel/link? Default to first.
+					if (!$panel
+					||	$panel.length == 0) {
+
+						$panel = $panels.first();
+						$link = $nav_links.first();
+
+					}
+
+				// Deactivate all panels except this one.
+					$panels.not($panel)
+						.addClass('inactive')
+						.hide();
+
+				// Activate link.
+					$link
+						.addClass('active');
+
+				// Reset scroll.
+					$window.scrollTop(0);
+
+			})();
+
+		// Hashchange event.
+			$window.on('hashchange', function(event) {
+
+				var $panel, $link;
+
+				// Get panel, link.
+					if (window.location.hash) {
+
+				 		$panel = $panels.filter(window.location.hash);
+						$link = $nav_links.filter('[href="' + window.location.hash + '"]');
+
+						// No target panel? Bail.
+							if ($panel.length == 0)
+								return;
+
+					}
+
+				// No panel/link? Default to first.
+					else {
+
+						$panel = $panels.first();
+						$link = $nav_links.first();
+
+					}
+
+				// Deactivate all panels.
+					$panels.addClass('inactive');
+
+				// Deactivate all links.
+					$nav_links.removeClass('active');
+
+				// Activate target link.
+					$link.addClass('active');
+
+				// Set max/min height.
+					$main
+						.css('max-height', $main.height() + 'px')
+						.css('min-height', $main.height() + 'px');
+
+				// Delay.
+					setTimeout(function() {
+
+						// Hide all panels.
+							$panels.hide();
+
+						// Show target panel.
+							$panel.show();
+
+						// Set new max/min height.
+							$main
+								.css('max-height', $panel.outerHeight() + 'px')
+								.css('min-height', $panel.outerHeight() + 'px');
+
+						// Reset scroll.
+							$window.scrollTop(0);
+
+						// Delay.
+							window.setTimeout(function() {
+
+								// Activate target panel.
+									$panel.removeClass('inactive');
+
+								// Clear max/min height.
+									$main
+										.css('max-height', '')
+										.css('min-height', '');
+
+								// IE: Refresh.
+									$window.triggerHandler('--refresh');
+
+								// Unlock.
+									locked = false;
+
+							}, (breakpoints.active('small') ? 0 : 500));
+
+					}, 250);
+
+			});
+
+	// IE: Fixes.
+		if (browser.name == 'ie') {
+
+			// Fix min-height/flexbox.
+				$window.on('--refresh', function() {
+
+					$wrapper.css('height', 'auto');
+
+					window.setTimeout(function() {
+
+						var h = $wrapper.height(),
+							wh = $window.height();
+
+						if (h < wh)
+							$wrapper.css('height', '100vh');
+
+					}, 0);
+
+				});
+
+				$window.on('resize load', function() {
+					$window.triggerHandler('--refresh');
+				});
+
+			// Fix intro pic.
+				$('.panel.intro').each(function() {
+
+					var $pic = $(this).children('.pic'),
+						$img = $pic.children('img');
+
+					$pic
+						.css('background-image', 'url(' + $img.attr('src') + ')')
+						.css('background-size', 'cover')
+						.css('background-position', 'center');
+
+					$img
+						.css('visibility', 'hidden');
+
+				});
+
+		}
+
+})(jQuery);
+
+/*
+	Typewriter Effect
+	CSS-Tricks.com | Geoff Graham
+	It shares the code sinppets on this code for the text animation function
+	https://css-tricks.com/snippets/css/typewriter-effect/
+*/
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
